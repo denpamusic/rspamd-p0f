3f89625f23d9f852c364afd035e8d46e8b051ab5
denpamusic <denpa@netfleet.space>
2019-09-13 02:23:42 +0300

    Add result caching

--- README.md
+++ README.md
@@ -2,9 +2,6 @@
 
 This lua plugin provides passive OS fingerprinting feature for [rspamd](https://github.com/rspamd/rspamd) filtering system via [p0f v3](https://github.com/p0f/p0f) fingerprinter. This allows to (somewhat inaccurately) detect which operating system running on remote server and what type of connection it's using. This information can then be used as an additional factor in detecting infected PCs and botnets.
 
->:warning: As of now, usage of this plugin in high to medium load mail systems is strongly discouraged due to having no result caching whatsoever (will be implemented later via rspamd_redis)
-
-
 ## Running p0f with API support
 To make p0f listen to API requests on unix socket, you will need to run it with `-s file` flag set:  
 ```bash
@@ -22,21 +19,31 @@ service p0f start
 Move all files from __rspamd__ directory of this repository to your rspamd configuration directory (e. g. `/etc/rspamd` on Debian or `/usr/local/etc/rspamd` on FreeBSD) and reload rspamd.
 
 To make sure that everything works, look for P0F symbol in message scan results or X-OS-Fingerprint header:
-![P0F symbol](https://user-images.githubusercontent.com/16575433/64662404-056d4f00-d451-11e9-9f53-945147e3802e.png)
+![P0F symbol](https://user-images.githubusercontent.com/16575433/64826636-f8c03680-d5c9-11e9-86fc-b0ab1eda7ee4.png)
 
 Plugin can be configured by editing following settings in `/etc/rspamd/local.d/p0f.conf` _(must be created first)_
-```lua
-p0f {
-  # Path to the unix socket that p0f listens on
-  socket = '/tmp/p0f.sock';
+```
+# Path to the unix socket that p0f listens on
+socket = '/tmp/p0f.sock';
 
-  # Connection timeout
-  timeout = 10;
+# Connection timeout
+timeout = 10;
 
-  # If defined, insert symbol with lookup results
-  symbol = 'P0F';
+# If defined, insert symbol with lookup results
+symbol = 'P0F';
 
-  # If defined, insert header with lookup results
-  header = 'X-OS-Fingerprint';
+# If defined, insert header with lookup results with following format
+# format "$OS (up: $UPTIME min), (distance $DISTANCE, link: $LINK), [$IP]"
+header = false;
+
+# Patterns to match OS string against
+patterns = {
+  WINDOWS = '^Windows.*';
 }
+
+# Cache lifetime in seconds (default - 2 hours)
+expire = 7200;
+
+# Cache key prefix
+key_prefix = 'p0f';
 ```
--- rspamd/plugins.d/p0f.lua
+++ rspamd/plugins.d/p0f.lua
@@ -3,6 +3,7 @@ local tcp = require "rspamd_tcp"
 local rspamd_util = require "rspamd_util"
 local rspamd_logger = require "rspamd_logger"
 local rspamd_regexp = require "rspamd_regexp"
+local lua_redis = require "lua_redis"
 local lua_util = require "lua_util"
 
 -- SEE: https://github.com/p0f/p0f/blob/v3.06b/docs/README#L317
@@ -35,6 +36,12 @@ p0f {
   patterns = {
     WINDOWS = '^Windows.*';
   }
+
+  # Cache lifetime in seconds (default - 2 hours)
+  expire = 7200;
+
+  # Cache key prefix
+  key_prefix = 'p0f';
 }
 ]])
   return
@@ -45,13 +52,18 @@ local settings = {
   timeout = 10,
   header = false,
   symbol = 'P0F',
-  patterns = {}
+  patterns = {},
+  expire = 7200, -- 2 hours
+  key_prefix = 'p0f'
 }
 
 local templates = {
    hdr = '$OS (up: $UPTIME min), (distance $DISTANCE, link: $LINK), [$IP]',
    res = 'os: $OS, uptime: $UPTIME min, distance: $DISTANCE, link: $LINK'
 }
+
+local ip;
+local redis_params;
 
 local function ip2bin(ip)
   local addr = ip:to_table()
@@ -83,7 +95,28 @@ local function check_p0f(task)
       lua_util.template(templates.hdr, result))
   end
 
-  local function add_p0f(result)
+  local function parse_p0f_response(data)
+    --[[
+      p0f_api_response: magic, status, first_seen, last_seen, total_conn,
+      uptime_min, up_mod_days, last_nat, last_chg, distance, bad_sw, os_match_q,
+      os_name, os_flavor, http_name, http_flavor, link_type, language
+    ]]--
+
+    local _, status, _, _, _, uptime_min, _, _, _, distance, _, _, os_name,
+      os_flavor, _, _, link_type, _ = trim(rspamd_util.unpack(
+        'I4I4I4I4I4I4I4II4i1I1I1c32c32c32c32c32c32', tostring(data)))
+
+    return {
+      STATUS = status,
+      UPTIME = uptime_min,
+      DISTANCE = distance,
+      OS = #os_name ~= 0 and (os_name .. ' ' .. os_flavor) or 'unknown',
+      LINK = link_type,
+      IP = tostring(ip)
+    }
+  end
+
+  local function add_p0f_results(result)
     task:get_mempool():set_variable('os', result.OS)
 
     for sym, r in pairs(settings.patterns) do
@@ -105,49 +138,82 @@ local function check_p0f(task)
     end
   end
 
-  local function check_p0f_cb(err, data)
-    --[[
-      p0f_api_response: magic, status, first_seen, last_seen, total_conn,
-      uptime_min, up_mod_days, last_nat, last_chg, distance, bad_sw, os_match_q,
-      os_name, os_flavor, http_name, http_flavor, link_type, language
-    ]]--
+  local function make_p0f_request()
 
-    local _, status, _, _, _, uptime_min, _, _, _, distance, _, _, os_name,
-      os_flavor, _, _, link_type, _ = trim(rspamd_util.unpack(
-        'I4I4I4I4I4I4I4II4i1I1I1c32c32c32c32c32c32', tostring(data)))
+    local function check_p0f_cb(err, data)
 
-      if status ~= S.OK then
-        if status == S.BAD_QUERY then
+      local function redis_set_cb(err)
+        if err then
+          rspamd_logger.errx(task, 'redis received an error: %1', err)
+          return
+        end
+      end
+
+      local resp = parse_p0f_response(data)
+
+      if resp.STATUS ~= S.OK then
+        if resp.STATUS == S.BAD_QUERY then
           rspamd_logger.errx(task, 'malformed p0f query on %s', settings.socket)
         end
         return
       end
 
-      add_p0f({
-        OS = #os_name ~= 0 and (os_name .. ' ' .. os_flavor) or 'unknown',
-        UPTIME = uptime_min,
-        DISTANCE = distance,
-        LINK = link_type,
-        IP = tostring(task:get_from_ip())
-      })
+      add_p0f_results(resp)
+
+      local key = settings.key_prefix .. ip:to_string()
+      local ret = lua_redis.redis_make_request(task,
+        redis_params,
+        key,
+        true,
+        redis_set_cb,
+        'SETEX',
+        { key, tostring(settings.expire), tostring(data) }
+      )
+
+      if not ret then
+        rspamd_logger.warnx(task, 'error connecting to redis')
+      end
+    end
+
+    local query = rspamd_util.pack('I4 I1 c16', 0x50304601,
+      ip:get_version(), ip2bin(ip))
+
+    tcp.request({
+      host = settings.socket,
+      callback = check_p0f_cb,
+      data = { query },
+      task = task,
+      timeout = settings.timeout
+    })
   end
 
-  local ip = task:get_from_ip()
+  local function redis_get_cb(err, data)
+    if err or type(data) ~= 'string' then
+      make_p0f_request()
+    else
+      add_p0f_results(parse_p0f_response(data))
+    end
+  end
 
+  ip = task:get_from_ip()
   if not (ip and ip:is_valid()) or ip:is_local() then
     return
   end
 
-  local query = rspamd_util.pack('I4 I1 c16', 0x50304601,
-    ip:get_version(), ip2bin(ip))
+  local key = settings.key_prefix .. ip:to_string()
+  local ret = lua_redis.redis_make_request(task,
+    redis_params,
+    key,
+    false,
+    redis_get_cb,
+    'GET',
+    { key }
+  )
 
-  tcp.request({
-    host = settings.socket,
-    callback = check_p0f_cb,
-    data = { query },
-    task = task,
-    timeout = settings.timeout
-  })
+  if not ret then
+    rspamd_logger.warnx(task, 'error connecting to redis')
+    make_p0f_request() -- fallback to directly querying p0f
+  end
 end
 
 local id = rspamd_config:register_symbol({
@@ -187,5 +253,14 @@ if opts then
       parent = id,
       group = N
     })
+  end
+
+  redis_params = lua_redis.parse_redis_server(N)
+
+  if not redis_params then
+    lua_util.disable_module(N, 'redis')
+    rspamd_logger.errx(rspamd_config, 'no redis servers ' ..
+      'are specified, disabling module')
+    return
   end
 end
